9c3e52a66932c317c71fbeb74cbfb1dc1ef9eb18
ArchanaJebaraj <archuumca@gmail.com>
2024-05-14 19:47:50 +0300

    commit file

--- src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
+++ src/test/java/com/inetbanking/testCases/TC_LoginTest_001.java
@@ -13,6 +13,7 @@ public class TC_LoginTest_001 extends BaseClass {
 	@Test
 	public void loginTest() throws IOException {
 		test = extent.createTest("login test", "Login test");
+		test.info("Assert Test");
 		driver.get(baseURL);
 		//logger.info("URL is opened");
 		LoginPage lp=new LoginPage(driver);
@@ -21,7 +22,7 @@ public class TC_LoginTest_001 extends BaseClass {
 		lp.clickSubmit();
 		catureScreenShort(driver,"login page");
 		System.out.println(driver.getTitle());
-		
+		test.info("Assert Test");
 		if(driver.getTitle().equals("Guru99 Bank Manager HomePage")) {
 			Assert.assertTrue(true);
 		}
